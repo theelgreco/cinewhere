77d46f8fdee9fe6ddc69bcfca148552e957d22f8
theelgreco <stelios.kakoliris@yahoo.com>
2023-04-26 12:45:01 +0100

    rerouted / to /home

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,18 @@
 import "@/styles/globals.css";
+import styles from "@/styles/App.module.css";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  const router = useRouter();
+
+  useEffect(() => {
+    router.push("/home");
+  }, []);
+
+  if (router.asPath === "/") {
+    return <div className={styles.preload}></div>;
+  } else {
+    return <Component {...pageProps} />;
+  }
 }
