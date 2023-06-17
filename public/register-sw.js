80eca2bc585424d231f20777864a2fdfba5d6ae8
theelgreco <stelios.kakoliris@yahoo.com>
2023-06-17 14:03:16 +0100

    testing service worker mobile

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,10 +1,10 @@
 console.log(navigator);
-// if (navigator.serviceWorker.controller) {
-//   console.log("Active service worker found");
-// } else {
-//   navigator.serviceWorker
-//     .register("install-sw.js", { scope: "./" })
-//     .then((reg) => {
-//       console.log("Service worker  registered");
-//     });
-// }
+if (navigator.serviceWorker.controller) {
+  console.log("Active service worker found");
+} else {
+  navigator.serviceWorker
+    .register("install-sw.js", { scope: "./" })
+    .then((reg) => {
+      console.log("Service worker  registered");
+    });
+}
