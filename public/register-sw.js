ffdbccdb8c5856c329a33a8366f698515d9fe973
theelgreco <stelios.kakoliris@yahoo.com>
2023-06-18 16:31:16 +0100

    updated display manifest

--- public/register-sw.js
+++ public/register-sw.js
@@ -1,10 +1,7 @@
 console.log(navigator);
-if (navigator.serviceWorker.controller) {
-  console.log("Active service worker found");
-} else {
-  navigator.serviceWorker
-    .register("install-sw.js", { scope: "./" })
-    .then((reg) => {
-      console.log("Service worker  registered");
-    });
-}
+
+navigator.serviceWorker
+  .register("install-sw.js", { scope: "./" })
+  .then((reg) => {
+    console.log("Service worker  registered");
+  });
