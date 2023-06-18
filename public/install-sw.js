b6ee32d8cbb5d5893fd71ec87cbb9d9cc472e1d1
theelgreco <stelios.kakoliris@yahoo.com>
2023-06-18 16:21:25 +0100

    updated display manifest

--- public/install-sw.js
+++ public/install-sw.js
@@ -1,12 +1,23 @@
-let CACHE_NAME = "my-cache-v3.0.1";
+let cacheName = "my-cache-v3.0.1";
 let urlsToCache = ["/manifest.json"];
 
 self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
-    caches.open(CACHE_NAME).then(function (cache) {
-      console.log("Opened cache");
-      return cache.addAll(urlsToCache);
-    })
+    caches
+      .delete(cacheName)
+      .then(() => {
+        console.log(`Old cache "${cacheName}" has been deleted.`);
+        return caches.open(cacheName);
+      })
+      .then((cache) => {
+        return cache.addAll(newCacheUrls);
+      })
+      .then(() => {
+        console.log(`New cache "${cacheName}" has been set.`);
+      })
+      .catch((error) => {
+        console.error("Cache update error:", error);
+      })
   );
 });
