84aa7cf882d01872c99a4baf4c49d768be75b55d
theelgreco <stelios.kakoliris@yahoo.com>
2023-06-18 16:21:47 +0100

    updated display manifest

--- public/activate-sw.js
+++ public/activate-sw.js
@@ -1,22 +1,14 @@
-const cacheName = "my-cache-v3.0.1";
-const newCacheUrls = ["/manifest.json"];
-
-self.addEventListener("activate", (event) => {
+self.addEventListener("activate", function (event) {
+  var cacheWhitelist = ["pigment"];
   event.waitUntil(
-    caches
-      .delete(cacheName)
-      .then(() => {
-        console.log(`Old cache "${cacheName}" has been deleted.`);
-        return caches.open(cacheName);
-      })
-      .then((cache) => {
-        return cache.addAll(newCacheUrls);
-      })
-      .then(() => {
-        console.log(`New cache "${cacheName}" has been set.`);
-      })
-      .catch((error) => {
-        console.error("Cache update error:", error);
-      })
+    caches.keys().then(function (cacheNames) {
+      return Promise.all(
+        cacheNames.map(function (cacheName) {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
   );
 });
