59b3da67a5221bd1b929f0582817ef251793ed83
theelgreco <stelios.kakoliris@yahoo.com>
2023-06-18 16:34:58 +0100

    updated display manifest

--- public/install-sw.js
+++ public/install-sw.js
@@ -1,23 +1,12 @@
-let cacheName = "my-cache-v3.0.1";
+let CACHE_NAME = "my-cache-v3.0.1";
 let urlsToCache = ["/manifest.json"];
 
 self.addEventListener("install", function (event) {
   // Perform install steps
   event.waitUntil(
-    caches
-      .delete(cacheName)
-      .then(() => {
-        console.log(`Old cache "${cacheName}" has been deleted.`);
-        return caches.open(cacheName);
-      })
-      .then((cache) => {
-        return cache.addAll(urlsToCache);
-      })
-      .then(() => {
-        console.log(`New cache "${cacheName}" has been set.`);
-      })
-      .catch((error) => {
-        console.error("Cache update error:", error);
-      })
+    caches.open(CACHE_NAME).then(function (cache) {
+      console.log("Opened cache");
+      return cache.addAll(urlsToCache);
+    })
   );
 });
--- public/register-sw.js
+++ public/register-sw.js
@@ -1,7 +1,11 @@
-console.log(navigator);
-
-navigator.serviceWorker
-  .register("install-sw.js", { scope: "./" })
-  .then((reg) => {
-    console.log("Service worker  registered");
-  });
+if (navigator.serviceWorker.controller) {
+  console.log("Active service worker found");
+  console.log(navigator.serviceWorker);
+} else {
+  navigator.serviceWorker
+    .register("install-sw.js", { scope: "./" })
+    .then((reg) => {
+      console.log(reg);
+      console.log("Service worker  registered");
+    });
+}
