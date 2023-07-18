4f6af68d51393752d420c7d6a9aab5742c039c8f
theelgreco <stelios.kakoliris@yahoo.com>
2023-07-18 16:06:52 +0100

    added cast

--- components/MovieInfo.jsx
+++ components/MovieInfo.jsx
@@ -7,7 +7,7 @@ import Poster from "./subcomponents/Poster";
 import ServicesTable from "./subcomponents/ServicesTable";
 import { getFilmByIdTmdb } from "api";
 import { getOfficialTrailer } from "utils/utils";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useRef } from "react";
 import { useParams } from "react-router-dom";
 
 export default function MovieInfo({ isMobile, options, region }) {
@@ -15,6 +15,10 @@ export default function MovieInfo({ isMobile, options, region }) {
   const [film, setFilm] = useState(null);
   const [trailer, setTrailer] = useState(null);
   const [layout, setLayout] = useState(updateLayout(window.innerWidth));
+  const [cast, setCast] = useState(null);
+  const [crew, setCrew] = useState(null);
+  const [castPage, setCastPage] = useState(1);
+  const [crewPage, setCrewPage] = useState(1);
 
   useEffect(() => {
     getFilmByIdTmdb(imdb_id, media_type).then((res) => {
@@ -24,6 +28,8 @@ export default function MovieInfo({ isMobile, options, region }) {
         setFilm(res);
         console.log(res);
         setTrailer(getOfficialTrailer(res));
+        setCast(paginate(res.credits.cast));
+        setCrew(paginate(res.credits.crew));
       }
     });
 
@@ -44,6 +50,24 @@ export default function MovieInfo({ isMobile, options, region }) {
     } else {
       return 1;
     }
+  }
+
+  function paginate(arr) {
+    if (!arr.length) return { 1: [] };
+
+    let obj = {};
+    let page = 1;
+
+    let resArr = [];
+    for (let i = 0; i < arr.length; i++) {
+      if (i % 10 === 0 && i > 0) {
+        obj[page] = resArr;
+        resArr = [];
+        page++;
+      }
+      resArr.push(arr[i]);
+    }
+    return obj;
   }
 
   if (film === "doesn't exist") return <h1>BAD LUCK</h1>;
@@ -102,6 +126,88 @@ export default function MovieInfo({ isMobile, options, region }) {
               {layout === 2 ? (
                 <div className={styles.column + " " + styles.cast}>
                   <h3>Cast</h3>
+                  <div className={styles.wrapper}>
+                    <div className={styles.flex}>
+                      {cast[castPage - 1] ? (
+                        <p
+                          style={{
+                            position: "absolute",
+                            left: "15px",
+                            top: "50%",
+                            translate: "0 -50%",
+                          }}
+                          onClick={() => {
+                            const prevPage = castPage - 1;
+                            setCastPage(prevPage);
+                          }}>
+                          {"<"}
+                        </p>
+                      ) : (
+                        <></>
+                      )}
+                      {cast[castPage].map((member) => {
+                        return (
+                          <div
+                            key={`${member.name}${member.character}`}
+                            style={{
+                              display: "flex",
+                              flexDirection: "column",
+                              alignItems: "center",
+                              justifyContent: "center",
+                              textAlign: "center",
+                              fontSize: "6px",
+                              width: "15%",
+                              padding: "5px",
+                            }}>
+                            {member.profile_path ? (
+                              <div
+                                style={{
+                                  width: "80%",
+                                  backgroundImage: `url(https://image.tmdb.org/t/p/w500${member.profile_path})`,
+                                  aspectRatio: "1",
+                                  backgroundSize: "cover",
+                                  backgroundPosition: "center",
+                                  borderRadius: "9999px",
+                                  margin: "0 auto",
+                                }}></div>
+                            ) : (
+                              <div
+                                style={{
+                                  width: "80%",
+                                  background: `rgb(0 0 0 /50%)`,
+                                  aspectRatio: "1",
+                                  backgroundSize: "cover",
+                                  backgroundPosition: "center",
+                                  borderRadius: "9999px",
+                                  margin: "0 auto",
+                                }}></div>
+                            )}
+                            <p className={styles.castText}>{member.name}</p>
+                            <p className={styles.castText}>
+                              {member.character}
+                            </p>
+                          </div>
+                        );
+                      })}
+                      {cast[castPage + 1] ? (
+                        <p
+                          style={{
+                            position: "absolute",
+                            right: "15px",
+                            top: "50%",
+                            translate: "0 -50%",
+                          }}
+                          onClick={() => {
+                            const nextPage = castPage + 1;
+                            setCastPage(nextPage);
+                          }}>
+                          {">"}
+                        </p>
+                      ) : (
+                        <></>
+                      )}
+                    </div>
+                  </div>
                 </div>
               ) : (
                 <></>
@@ -118,6 +224,7 @@ export default function MovieInfo({ isMobile, options, region }) {
             </div>
             {layout === 2 ? (
               <div className={styles.column + " " + styles.services}>
+                <h3>Where to watch</h3>
                 <ServicesTable
                   services={film["watch/providers"]["results"][region]}
                   filmTitle={film.title || film.name}
@@ -141,7 +248,84 @@ export default function MovieInfo({ isMobile, options, region }) {
               <div className={styles.column + " " + styles.cast}>
                 <h3>Cast</h3>
                 <div className={styles.wrapper}>
-                  <div className={styles.flex}></div>
+                  <div className={styles.flex}>
+                    {cast[castPage - 1] ? (
+                      <p
+                        style={{
+                          position: "absolute",
+                          left: "15px",
+                          top: "50%",
+                          translate: "0 -50%",
+                        }}
+                        onClick={() => {
+                          const prevPage = castPage - 1;
+                          setCastPage(prevPage);
+                        }}>
+                        {"<"}
+                      </p>
+                    ) : (
+                      <></>
+                    )}
+                    {cast[castPage].map((member) => {
+                      return (
+                        <div
+                          key={`${member.name}${member.character}`}
+                          style={{
+                            display: "flex",
+                            flexDirection: "column",
+                            alignItems: "center",
+                            justifyContent: "center",
+                            textAlign: "center",
+                            fontSize: "10px",
+                            width: "15%",
+                            padding: "10px",
+                          }}>
+                          {member.profile_path ? (
+                            <div
+                              style={{
+                                width: "80%",
+                                backgroundImage: `url(https://image.tmdb.org/t/p/w500${member.profile_path})`,
+                                aspectRatio: "1",
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                borderRadius: "9999px",
+                                margin: "0 auto",
+                              }}></div>
+                          ) : (
+                            <div
+                              style={{
+                                width: "80%",
+                                background: `rgb(0 0 0 /50%)`,
+                                aspectRatio: "1",
+                                backgroundSize: "cover",
+                                backgroundPosition: "center",
+                                borderRadius: "9999px",
+                                margin: "0 auto",
+                              }}></div>
+                          )}
+                          <p className={styles.castText}>{member.name}</p>
+                          <p className={styles.castText}>{member.character}</p>
+                        </div>
+                      );
+                    })}
+                    {cast[castPage + 1] ? (
+                      <p
+                        style={{
+                          position: "absolute",
+                          right: "15px",
+                          top: "50%",
+                          translate: "0 -50%",
+                        }}
+                        onClick={() => {
+                          const nextPage = castPage + 1;
+                          setCastPage(nextPage);
+                        }}>
+                        {">"}
+                      </p>
+                    ) : (
+                      <></>
+                    )}
+                  </div>
                 </div>
               </div>
             </>
