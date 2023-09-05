b0ca184f1656e67f279e4067be37639501be47a6
theelgreco <stelios.kakoliris@yahoo.com>
2023-09-05 17:59:57 +0100

    debugging

--- components/subcomponents/Trailer.jsx
+++ components/subcomponents/Trailer.jsx
@@ -12,6 +12,7 @@ export default function Trailer({ trailerPlaying, trailer }) {
   async function getVideoElement() {
     const baseURL = window.location.origin;
     try {
+      console.log("trying");
       const res = await axios.get(`${baseURL}/api/video/${trailer}`);
 
       const videoElementString = res.data;
