5c9e676abe30e2e2093902bd349ddf93c5981ccb
Stelios Kakoliris <stelios.kakoliris@yahoo.com>
2026-03-18 09:06:14 +0000

    Integrate Google Analytics into index.js
    Added Google Analytics script for tracking.

--- pages/index.js
+++ pages/index.js
@@ -69,6 +69,18 @@ export default function Home() {
   return (
     <BrowserRouter>
       <Head>
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-VGSDRJFYF7"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-VGSDRJFYF7');
+          `}
+        </Script>
         <title>
           Discover where you can watch your favourite movies and tv shows
         </title>
